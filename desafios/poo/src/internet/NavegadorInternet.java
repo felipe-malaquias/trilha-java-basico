d58b2fe3a9e976cec96b6669ec080f9b3a835216
felipe-malaquias <157316361+felipe-malaquias@users.noreply.github.com>
2024-10-21 21:00:07 -0300

    Add files via upload

---
+++ desafios/poo/src/internet/NavegadorInternet.java
@@ -0,0 +1,8 @@
+package desafio_poo.internet;
+
+public interface NavegadorInternet {
+	
+	public void exibirPagina(String url);
+	public void adicionarNovaAba();
+	public void atualizarPagina();
+}
